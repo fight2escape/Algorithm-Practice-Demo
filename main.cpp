10de9941149cf78c1e9c64590b07111f80e54efb
DESKTOP-P4K92SG\Shinelon <fight2escape@gmail.com>
2017-09-16 00:31:57 +0800

    图论基础：稀疏/稠密图，连通分量，寻路算法，读取文件，DFS,BFS

--- main.cpp
+++ main.cpp
@@ -1,12 +1,37 @@
 
 #include <iostream>
+#include <vector>
+#include <ctime>
+#include "DenseGraph.h"
+#include "ReadGraph.h"
+#include "SparseGraph.h"
+#include "Component.h"
+#include "Path.h"
+#include "ShortestPath.h"
 
 using namespace std;
 
 int main() {
-    cout<<"Hello World"<<endl;
+    string filename = "testG2.txt";
+    SparseGraph g = SparseGraph( 6 , false );
+    ReadGraph<SparseGraph> readGraph1( g , filename );
+    g.show();
+    cout<<endl;
 
+    Path<SparseGraph> dfs(g,0);
+    cout<<"DFS : ";
+    dfs.showPath(5);
 
-    cout<<"test"<<endl;
+    ShortestPath<SparseGraph> bfs(g, 0);
+    cout<<"BFS : ";
+    bfs.showPath(5);
+
+//    DenseGraph g2(13, false);
+//    ReadGraph<DenseGraph> readGraph2(g2, filename);
+//    cout<<"test G2 in Sparse Graph:"<<endl;
+////    g2.show();
+//    Component<DenseGraph> component2(g2);
+//    cout<<"count:"<<component2.count()<<endl;
+
     return 0;
 }
