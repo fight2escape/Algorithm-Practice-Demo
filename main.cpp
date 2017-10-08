6723fcf98d911927eaec8e39fcae3ad35162f049
DESKTOP-P4K92SG\Shinelon <fight2escape@gmail.com>
2017-10-08 20:14:25 +0800

    选择排序

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
-
 #include <iostream>
-
 using namespace std;
 
+template<typename T>
+void selectionSort(T* arr, int n){
+    for(int i=0; i<n; i++){
+        int pos = i;
+        for(int j=i+1; j<n; j++)
+            if(arr[j] < arr[i])
+                pos = j;
+        if(i != pos)
+            swap(arr[i], arr[pos]);
+    }
+}
+
+
 int main() {
-    
+    int arr[] = {1,2,3,4};
+    int n = sizeof(arr)/sizeof(int);
+    selectionSort(arr, n);
+    for(auto a : arr){
+        cout<<a<<" ";
+    }
     return 0;
 }
